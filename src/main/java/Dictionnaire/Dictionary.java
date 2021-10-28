39fff94d9867d242af9f9712cf1d293b2e51c51d
LydiaSM <l.smahi@hotmail.fr>
2021-10-28 12:03:24 +0200

    Question 4

---
+++ src/main/java/Dictionnaire/Dictionary.java
@@ -0,0 +1,19 @@
+package Dictionnaire;
+
+public class Dictionary {
+
+    private String name ;
+
+    public Dictionary() {
+        this.name = "Exampleee";
+    }
+    public String getName()
+    {
+        return this.name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+}
---
+++ src/test/java/Dictionnaire/DictionaryTest.java
@@ -0,0 +1,22 @@
+package Dictionnaire;
+
+import org.testng.annotations.Test;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@Test
+public class DictionaryTest {
+
+    private Dictionary dict;
+
+    public DictionaryTest()
+    {
+        this.dict = new Dictionary();
+    }
+
+    @Test
+    public void testDictionaryName() {
+        assertThat(dict.getName(), equalTo("Example"));
+    }
+
+}
