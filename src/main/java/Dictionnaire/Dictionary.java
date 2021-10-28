f03e065b930dc8197c8c4c89629c1848f75c21e1
LydiaSM <l.smahi@hotmail.fr>
2021-10-28 12:39:41 +0200

    Question 7

--- src/main/java/Dictionnaire/Dictionary.java
+++ src/main/java/Dictionnaire/Dictionary.java
@@ -2,17 +2,18 @@ package Dictionnaire;
 
 public class Dictionary {
 
-    private String name ;
+    private String name;
 
     public Dictionary() {
         this.name = "Example";
     }
 
+    //Question 4
     public String getName()
     {
         return this.name;
     }
-
+    //Question 7
     public boolean isEmpty()
     {
         return true;
--- src/test/java/Dictionnaire/DictionaryTest.java
+++ src/test/java/Dictionnaire/DictionaryTest.java
@@ -24,6 +24,4 @@ public class DictionaryTest {
         assertThat(dict.isEmpty(), equalTo(true));
     }
 
-
-
 }
